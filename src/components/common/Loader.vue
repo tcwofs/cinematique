2a4c8a61746cd5b7cc97c82ef14e12f72ef36bd7
tcwofs <tcwofs@gmail.com>
2022-11-01 15:08:36 +0200

    feat: add loader

---
+++ src/components/common/Loader.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { mdiMovieRoll } from '@mdi/js';
+import Icon from './Icon.vue';
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <Icon v-bind="$attributes" class="animate-spin" :d="mdiMovieRoll" />
+    <span>Loading...</span>
+  </div>
+</template>
