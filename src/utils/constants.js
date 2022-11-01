9985cf0870006e4efc1aaf6510ce805428efe1d5
tcwofs <tcwofs@gmail.com>
2022-11-01 20:33:18 +0200

    fix: add missing genres constants

---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const genres = [
+  { id: 0, title: 'action' },
+  { id: 1, title: 'adventures' },
+  { id: 2, title: 'comedy' },
+  { id: 3, title: 'drama' },
+  { id: 4, title: 'horror' },
+  { id: 5, title: 'westerns' },
+];
