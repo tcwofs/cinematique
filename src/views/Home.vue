6832642c665f940c001a35d493e33b77dba12c4f
tcwofs <tcwofs@gmail.com>
2022-11-01 12:41:27 +0200

    feat: update home view

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { mdiMovieRoll, mdiMovieSearchOutline } from '@mdi/js';
+import HomeLink from '../components/Actions/Links/HomeLink.vue';
+
+const links = [
+  { to: '/movies', icon: mdiMovieSearchOutline, text: 'movies' },
+  { to: '/sessions', icon: mdiMovieRoll, text: 'sessions' },
+];
+</script>
+
+<template>
+  <main
+    class="h-full w-full p-7 flex flex-col gap-24 items-center justify-center"
+  >
+    <h1 class="text-5xl md:text-7xl lg:text-9xl">cinematique</h1>
+    <div class="flex flex-col lg:flex-row items-center justify-center gap-7">
+      <HomeLink
+        v-for="link in links"
+        :key="link.to"
+        :to="link.to"
+        :icon="link.icon"
+        :text="link.text"
+      />
+    </div>
+  </main>
+</template>
