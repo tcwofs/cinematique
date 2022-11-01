63b40196554a6163e9daf6854bef2f624d46e8a5
tcwofs <tcwofs@gmail.com>
2022-11-01 12:40:44 +0200

    feat: add new components

---
+++ src/components/Actions/Links/HomeLink.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+import Icon from '../../common/Icon.vue';
+
+const props = defineProps({
+  to: { type: String, required: true },
+  icon: { type: String, default: '' },
+  text: { type: String, default: '' },
+});
+</script>
+
+<template>
+  <RouterLink
+    :to="props.to"
+    class="btn h-32 w-64 lg:w-96 lg:h-64 flex-shrink flex"
+  >
+    <span
+      class="flex flex-col text-3xl font-normal text-base-100 my-auto lowercase"
+    >
+      <Icon v-if="props.icon" class="w-24 h-24 m-auto" :d="props.icon" />
+      <span v-if="props.text">{{ props.text }}</span>
+    </span>
+  </RouterLink>
+</template>
---
+++ src/components/common/Icon.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  d: { type: String, required: true },
+  viewBox: { type: String, default: '0 0 24 24' },
+  size: { type: [Number, String], default: 6 },
+});
+</script>
+
+<template>
+  <svg
+    :class="`h-${props.size} w-${props.size}`"
+    aria-hidden="true"
+    :viewBox="viewBox"
+  >
+    <path fill="currentColor" :d="props.d" />
+  </svg>
+</template>
